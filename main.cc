14b6883dc90e81ab1be7a0a8b979193a954ee15f
Juha Kankare <juhakankare@outlook.com>
2020-03-02 15:45:49 +0200

    merge?

--- main.cc
+++ main.cc
@@ -5,7 +5,6 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-<<<<<<< HEAD
     int pid;
     if((pid = fork()) == 0) {
         execl("/bin/cat", "/bin/cat", "/etc/meg.txt", (char*)NULL);
@@ -22,26 +21,5 @@ int main(int argc, char** argv) {
     argv[argc] = NULL;
     execv("/bin/init",argv2);
     printf("crash\n");
-=======
-    execl("/bin/cat","/etc/meg.txt");
-    sleep(5);
-    printf("%d\n",unshare(CLONE_NEWPID));
-    int pid = fork();
-    printf("%d\n",getpid());
-    if(getpid() == 1) {
-        char** argv2 = (char**)malloc(sizeof(char)*argc);
-        argv2[0] = (char*)malloc(sizeof(char)*5);
-        argv2[0] = "init\00";
-        for(int i = 1; i < argc; i++) {
-            argv2[i-1] = argv[i];
-            printf("%s\n",argv[i]);
-        }
-        argv[argc] = NULL;
-        execv("/bin/init",argv2);
-    }
-    else {
-        waitpid(-1,NULL,0);
-    }
->>>>>>> 228df04
     return 0;
 }
